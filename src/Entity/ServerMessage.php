accc6de8d75b4e95674a2be20e57c264502012ed
github-ci <you@example.com>
2025-04-28 09:46:00 +0000

    fix: 修复字段错误

--- src/Entity/ServerMessage.php
+++ src/Entity/ServerMessage.php
@@ -26,7 +26,7 @@ class ServerMessage
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[Keyword]
     #[ListColumn]
